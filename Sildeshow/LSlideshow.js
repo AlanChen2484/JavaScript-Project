f2daaf6467af794ae2e7b9f213ba852e67a62ae0
AlanChen2484 <248466112@qq.com>
2017-06-09 22:03:37 +0800

    start appaction

---
+++ Sildeshow/LSlideshow.js
@@ -0,0 +1,5 @@
+var LSlideshow = function(){throw "LSlideshow cannot be instantiated";};
+LSlideshow.type = "LSlideshow";
+LSlideshow.RIGHTWARD = "rightward";
+LSlideshow.LEFTWARD = "leftward";
+LSlideshow.RANDOM = "random";
